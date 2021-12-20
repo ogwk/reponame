2f2f3179d799e88d9ff3c3bbbc0ed253b8f847bb
小川佳納 <ogwk@ogawakeiosamenoMacBook-Pro.local>
2021-12-21 07:50:33 +0900

    “add auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "reponameecfc73af": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
